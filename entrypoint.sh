4cd94a245b874fb458a1695c145c9ca2ca60c786
Malik Hattabi <kilam8900@gmail.com>
2022-12-16 22:45:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ apt update && apt upgrade -y
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
     curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
-    device-tree-compiler ca-certificates python3 python2
+    device-tree-compiler ca-certificates python3 python2 xz-utils
 ln -sf "/usr/bin/python${python_version}" /usr/bin/python
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 msg "Installing toolchain..."
